51ebd3f53281b967df8bbcc1c046816e4908d711
centuryhacker <108914645+centuryhacker@users.noreply.github.com>
2022-07-08 00:37:10 -0400

    Create  Write a program that produces the following output:.cpp
    /*****************************************************************************
     Write a program that produces the following output:
    **********************************
    *   Programming Assignment 1     *
    *    Computer Programming 1      *
    *         Author: ???            *
    *   Due Date: Thursday Jan. 24   *
    **********************************
    *****************************************************************************/

---
+++ Write a program that produces the following output:.cpp
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ Write a program that produces the following output:
+**********************************
+*   Programming Assignment 1     *
+*    Computer Programming 1      *
+*         Author: ???            *
+*   Due Date: Thursday Jan. 24   *
+**********************************
+*****************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  cout << "**********************************" << endl;
+  cout << "*" << setw(28) << "Programming Assignment 1" << setw(5) << "*" << endl;
+  cout << "*" << setw(27) << "Computer Programming 1" << setw(6) << "*" << endl;
+  cout << "*" << setw(21) << "Author: ???" << setw(12) << "*" << endl;
+  cout << "*" << setw(29) << "Due Date: Thursday Jan. 24" << setw(4) << "*" << endl;
+  cout << "**********************************" << endl;
+
+  return 0;
+}
